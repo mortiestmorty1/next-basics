c474d2da76a96b40ed9940f84e924611c6a829d6
mortiestmorty1 <shoaibahmed@Shoaibs-MacBook-Pro.local>
2025-04-24 17:08:14 +0500

    Replace bcrypt with bcryptjs for Vercel compatibility

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -3,7 +3,6 @@ import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
-import { lusitana } from '@/app/ui/fonts';
 
 export default async function InvoicesTable({
   query,

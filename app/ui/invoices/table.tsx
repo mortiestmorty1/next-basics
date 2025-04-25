1bcce78c40eed55e0ba656cfacad30cc99f01a9c
mortiestmorty1 <shoaibahmed@Shoaibs-MacBook-Pro.local>
2025-04-25 10:20:18 +0500

    Fix: add currentPage prop to InvoicesTable

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -6,8 +6,10 @@ import { fetchFilteredInvoices } from '@/app/lib/data';
 
 export default async function InvoicesTable({
   query,
+  currentPage,
 }: {
   query: string;
+  currentPage: number;
 }) {
   const invoices = await fetchFilteredInvoices(query);
 

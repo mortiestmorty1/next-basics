c333d6bdaf2aff40867536636d7a4f979b8dd9e4
mortiestmorty1 <shoaibahmed@Shoaibs-MacBook-Pro.local>
2025-04-25 10:53:12 +0500

    Add NextAuth route handler for credentials provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,52 @@
+// auth.config.ts
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcryptjs';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
+
+async function getUser(email: string): Promise<User | undefined> {
+  const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+  return user[0];
+}
 
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsed = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (!parsed.success) return null;
+
+        const { email, password } = parsed.data;
+        const user = await getUser(email);
+        if (!user) return null;
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return null;
+
+        return user;
+      },
+    }),
+  ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false;
-      } else if (isLoggedIn) {
+
+      if (isOnDashboard && !isLoggedIn) return false;
+      if (!isOnDashboard && isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
   },
-  providers: [], // Add providers here
 } satisfies NextAuthConfig;
